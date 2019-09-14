3c2be15da6e6694baf2821aa55f11cbfc2625649
scottxiong <github.scott.bear@gmail.com>
2019-09-14 15:30:48 +0800

    modify uuid format

--- util.go
+++ util.go
@@ -1,8 +1,8 @@
 /*
 * @Author: scottxiong
 * @Date:   2019-07-29 02:39:58
-* @Last Modified by:   sottxiong
-* @Last Modified time: 2019-07-29 04:33:50
+* @Last Modified by:   scottxiong
+* @Last Modified time: 2019-09-14 15:30:28
  */
 package oss
 
@@ -35,5 +35,5 @@ func newUUID() (string, error) {
 	uuid[8] = uuid[8]&^0xc0 | 0x80
 	// version 4 (pseudo-random); see section 4.1.3
 	uuid[6] = uuid[6]&^0xf0 | 0x40
-	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+	return fmt.Sprintf("%x%x%x%x%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
