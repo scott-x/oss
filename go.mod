4fa144d9c94f0e8c79a282b4a9cbc9a6da0ad165
scottxiong <github.scott.bear@gmail.com>
2019-09-14 14:51:18 +0800

    update

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/scott-x/oss
+
+go 1.12
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.0.2+incompatible
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+)
--- upload.go
+++ upload.go
@@ -1,8 +1,8 @@
 /*
 * @Author: scottxiong
 * @Date:   2019-07-29 02:29:25
-* @Last Modified by:   sottxiong
-* @Last Modified time: 2019-07-29 05:33:27
+* @Last Modified by:   scottxiong
+* @Last Modified time: 2019-09-14 14:47:23
  */
 package oss
 
@@ -13,18 +13,19 @@ import (
 	"strings"
 )
 
-func Upload(localFile string, uuid bool) {
+func Upload(localFile string, uuid bool) string {
 	var newFile string
 	s, _ := newUUID()
-	ext := path.Ext(strings.Trim(localFile," "))
+	ext := path.Ext(strings.Trim(localFile, " "))
 	if uuid {
 		newFile = s + ext
 	} else {
-		newFile = strings.Trim(localFile," ")
+		newFile = strings.Trim(localFile, " ")
 	}
 	fd, err := os.Open(localFile)
 	if err != nil {
 		fmt.Println("Error:", err)
+		return ""
 		os.Exit(-1)
 	}
 	defer fd.Close()
@@ -33,6 +34,38 @@ func Upload(localFile string, uuid bool) {
 	err = bucket.PutObject(newFile, fd)
 	if err != nil {
 		fmt.Println("Error:", err)
+		return ""
 		os.Exit(-1)
 	}
+	return "https://scott-x." + strings.Trim(configuration.Endpoint, "http://") + "/" + newFile
+}
+
+func UploadToSpecificFolder(localFile string, uuid bool, desFolder string) string {
+	var newFile string
+	s, _ := newUUID()
+	ext := path.Ext(strings.Trim(localFile, " "))
+	if uuid {
+		newFile = s + ext
+	} else {
+		newFile = strings.Trim(localFile, " ")
+	}
+	fd, err := os.Open(localFile)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return ""
+		os.Exit(-1)
+	}
+	defer fd.Close()
+
+	if !strings.Contains(desFolder, "/") {
+		desFolder = desFolder + "/"
+	}
+	// 上传文件流。
+	err = bucket.PutObject(desFolder+newFile, fd)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return ""
+		os.Exit(-1)
+	}
+	return "https://scott-x." + strings.Trim(configuration.Endpoint, "http://") + "/" + desFolder + newFile
 }
