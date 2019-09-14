f5b503c964051f92885b441483e4b707eeede1bb
scottxiong <github.scott.bear@gmail.com>
2019-09-14 15:25:55 +0800

    update

--- upload.go
+++ upload.go
@@ -2,7 +2,7 @@
 * @Author: scottxiong
 * @Date:   2019-07-29 02:29:25
 * @Last Modified by:   scottxiong
-* @Last Modified time: 2019-09-14 15:16:46
+* @Last Modified time: 2019-09-14 15:25:35
  */
 package oss
 
@@ -84,5 +84,21 @@ func UploadToSpecificFolder(localFile string, uuid bool, desFolder string) strin
 		return ""
 		os.Exit(-1)
 	}
-	return "https://scott-x." + strings.Trim(configuration.Endpoint, "http://") + "/" + desFolder + newFile
+	f, err := os.Open("./oss-conf.json")
+	if err != nil {
+		fmt.Printf("Open configuration error: %s\n", err)
+		return ""
+	}
+	defer f.Close()
+	configuration := &conf{}
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(configuration)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println(configuration)
+
+	endpoint := configuration.Endpoint
+	bucketName := configuration.BucketName
+	return "https://" + bucketName + "." + strings.Trim(endpoint, "http://") + "/" + desFolder + newFile
 }
